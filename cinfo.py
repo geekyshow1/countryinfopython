a646947088926ba9bdb0b2f62b57983a3e696f9b
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-06-16 14:50:35 +0530

    Add files via upload

---
+++ cinfo.py
@@ -0,0 +1,10 @@
+from countryinfo import CountryInfo
+
+country = CountryInfo('India')
+
+# data = country.alt_spellings()
+# data = country.area()
+# data = country.borders()
+data = country.info()
+
+print(data)
